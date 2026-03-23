f52c09fdf644ee6361c290055db002e14ca58a71
neiljohnson1306 <neil.johnson1306@gmail.com>
2026-03-23 13:25:31 +0000

    Update MainLayout.jsx

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,7 +1,10 @@
+import Navbar from "../components/Navbar"
+
 export default function MainLayout({ children }) {
   return (
     <div className="bg-[#020617] text-white min-h-screen">
-      {children}
+      <Navbar />
+      <div className="pt-20">{children}</div>
     </div>
   )
 }
