d5f714d356e4a6f65e5e1271ccacb44fc90d5c76
neiljohnson1306 <neil.johnson1306@gmail.com>
2026-03-23 11:29:56 +0000

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ export default function Navbar() {
 
         {/* Logo */}
         <Link to="/" className="flex flex-col leading-tight">
-          <span className="text-xl font-bold tracking-wide">LIBERTY</span>
+          <span className="text-xl font-bold tracking-wide">Cube IT</span>
           <span className="text-xs tracking-widest text-secondary dark:text-gray-400">
             CONSULTANCY
           </span>
